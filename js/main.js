7fc87ac35937c7d5d8790439a642654d818e5c4c
박성호 <parkho@bagseonghoui-MacBook-Pro-2.local>
2020-05-22 23:54:11 +0900

    end

--- js/main.js
+++ js/main.js
@@ -7,26 +7,26 @@ window.onload = function() {
 	gap = Math.floor(gap / (1000 * 60 * 60 * 24));
 	console.log(gap);
 	$("#d-day").html("D + " + gap);
-	
+
 	$('.i-checks').iCheck({
                 checkboxClass: 'icheckbox_square-green',
                 radioClass: 'iradio_square-green'
             });
-			
+
 	 $(".todo-list").sortable({
 		placeholder: "sort-highlight",
 		handle: ".handle",
 		forcePlaceholderSize: true,
 		zIndex: 999999
 	}).disableSelection();
-	
+
   let player = new cplayer({
 	element: document.getElementById('music_player'),
 	playlist: [
 	  {
 		src: './music/Beautiful_lyrics.mp3',
-		poster: './img/sungho.jpg',
-		name: 'Beautiful lyrics',
+		poster: './img/2.jpeg',
+		name: 'Beautiful',
 		artist: 'Crush',
 		//lyric: '[00:00.00]테스트\n[00:01.00]테스트1\n',
 		//sublyric: '[00:00.00]테스트\n[00:01.00]테스트1\n',
@@ -36,6 +36,6 @@ window.onload = function() {
 	big: true,
 	autoplay: true
   })
-	
+
 	//$(".todo-list").sortable();
 }
